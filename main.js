5b178402451a8070eea32edd44197b8a772cdc25
Milagros Ferro <milagrossferro@gmail.com>
2022-07-06 19:29:41 -0300

    LoopJs

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-//comentario
-/*comentaro de varias lineas
-*/
+let libro = prompt ("Ingresa el titulo de un libro") ;
 
+while (libro != "ESC" ){
+    alert("Ingresaste " + libro);
 
+    libro = prompt ("Ingresa otro, en caso de no querer escribi ESC");
+}
