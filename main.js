65485c9c605aa5e8376151d556d0bc441db13e6e
Milagros Ferro <milagrossferro@gmail.com>
2022-07-06 19:40:33 -0300

    LoopJS

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ let libro = prompt ("Ingresa el titulo de un libro") ;
 while (libro != "ESC" ){
     alert("Ingresaste " + libro);
 
-    libro = prompt ("Ingresa otro, en caso de no querer escribi ESC");
+    libro = prompt ("Ingresa otro, si no queres hacerlo deberas escribi ESC");
 }
