d6932bf7836e634f22a3e20a1ac6f76cee83ff51
Milagros Ferro <milagrossferro@gmail.com>
2022-07-11 14:42:31 -0300

    Simulador Interactivo

--- main.js
+++ main.js
@@ -1,7 +1,70 @@
-let libro = prompt ("Ingresa el titulo de un libro") ;
+/*let libro = prompt ("Ingresa el titulo de un libro") ;
 
 while (libro != "ESC" ){
     alert("Ingresaste " + libro);
 
     libro = prompt ("Ingresa otro, si no queres hacerlo deberas escribi ESC");
+}*/
+calcula_precio()
+
+function calcula_precio() {
+    let nombre= prompt("Ingresa tu nombre");
+    if (nombre != "ESC" ){
+        alert(`Estos son nuestros libros en stock ,  ${nombre}.`+
+        " 1 Reino de papel, "+
+        " 2 Heartstopper, "+
+        " 3 De sangre y cenizas. ");
+        let precio= prompt("Ingresa el numero listado del libro a consultar. Si no quieres hacerlo esribe 'ESC'");
+        while (precio != "ESC". toUpperCase ){
+            switch(precio.toUpperCase()){
+                case "1":
+                    alert( "El precio de Reino de papel por unidad es de $2.299,00");
+                    const rdp = prompt("Ingresa la cantidad de unidades deseas")
+                    total(rdpPrecio(rdp));
+                    break;
+
+                case "2":
+                    alert( "El precio de Heartstopper por unidad es de $1.999,00");
+                    const heartstopper = prompt("Ingresa la cantidad de unidades deseas")
+                    total(heartstopperPrecio(heartstopper));
+                    break;
+                    
+                case "3":
+                    alert( "El precio de De sangre y cenizas por unidad es de $1.999,00");
+                    const dsc = prompt("Ingresa la cantidad de unidades deseas")
+                    total(dscPrecio(dsc));
+                    break;   
+                    
+                default:
+                    alert("Actualmente no tenemos stock");
+                    break;    
+            }precio= prompt ("Si quieres saber el valor de otro libro vuelve a seleccionar el numero listado"+
+            " 1 Reino de papel, "+
+            " 2 Heartstopper, "+
+            " 3 De sangre y cenizas. "+
+            "Si ya terminaste tu consulta escribe `ESC`");
+        }
+    }else{
+        alert("Gracias por la consulta")
+    }
+
+}
+
+function rdpPrecio (rdp){
+    return rdp * 2299;
+
+}
+
+function heartstopperPrecio (heartstopper){
+    return heartstopper * 1999;
+
+}
+
+function dscPrecio (dsc){
+    return dsc * 2299;
+
+}
+ 
+function total (cantidades){
+    alert(`Tu total es de $ ${cantidades}`)
 }
